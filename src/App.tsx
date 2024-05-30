00017b6cae1904ec32f101239d8e3d5e2e04e100
egamovj <egamovj90@gmail.com>
2024-05-30 11:58:13 +0500

    header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import Why from "./components/Why"
 
 const App = () => {
   return (
     <div className="w-full flex flex-col">
       <Header />
+      <Why />
     </div>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import bg from "../../assets/images/bg2.png";
+import bag from "../../assets/images/bag.png";
 
 const Header = () => {
   return (
@@ -36,7 +37,18 @@ const Header = () => {
             Contact
           </a>
         </div>
+        <img src={bag} alt="cart" className="cursor-pointer w-[25px]" />
       </div>
+      <div className="w-full flex flex-col gap-5 items-center justify-center md:mt-[100px] mt-5">
+        <div className="text-center text-white font-bold md:text-[80px] text-[60px] md:max-w-[900px] leading-[90px]">
+          Make Your Interior More Minimalistic & Modern
+        </div>
+        <p className="text-white md:text-[24px] text-[18px] font-[200] max-w-[600px] text-center">
+          Turn your room with panto into a lot more minimalist and modern with
+          ease and speed
+        </p>
+      </div>
+      <div className="w-full h-[80px] bg-gradient-to-t from-white to-transparent absolute bottom-0"></div>
     </div>
   );
 };
